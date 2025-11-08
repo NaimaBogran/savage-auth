19edef8a227ed8ebaccfb3e99517cd60ada6779d
Naima e Bogran <Naima.E.Bogran@gmail.com>
2025-11-08 16:04:35 -0500

    complete

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
+
+console.log('Thumbs down elements found:', thumbDown.length)
 
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
@@ -22,6 +25,36 @@ Array.from(thumbUp).forEach(function(element) {
           console.log(data)
           window.location.reload(true)
         })
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+      element.addEventListener('click', function(){
+        console.log('Thumbs down clicked')
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        console.log('Name:', name, 'Msg:', msg, 'Current thumbUp:', thumbUp)
+        fetch('messagesDown', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          console.log('Respose status:', response.status)
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+        .catch(error => {
+          console.log('Error:', error)
+        });
       });
 });
 
